4c545d719abdcbd9dcfdfd5afd096f4362203dd7
Nicolai Danielsen <NICD@LB.DK>
2018-05-14 10:49:23 +0200

    ..

---
+++ lb-operations-ticker/src/webparts/lbOperationsTicker/components/ILbOperationsTickerProps.ts
@@ -0,0 +1,3 @@
+export interface ILbOperationsTickerProps {
+  description: string;
+}
---
+++ lb-operations-ticker/src/webparts/lbOperationsTicker/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ILbOperationsTickerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'LbOperationsTickerWebPartStrings' {
+  const strings: ILbOperationsTickerWebPartStrings;
+  export = strings;
+}
