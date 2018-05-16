af1119c41fa42a5d918ea5c7651eda3fefdeae03
Nicolai Danielsen <NICD@LB.DK>
2018-05-16 08:29:26 +0200

    --

---
+++ lb-myfavourites-extension/src/extensions/lbMyfavouritesExtension/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ILbMyfavouritesExtensionApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'LbMyfavouritesExtensionApplicationCustomizerStrings' {
+  const strings: ILbMyfavouritesExtensionApplicationCustomizerStrings;
+  export = strings;
+}
