7dfd95cff522ac116d3b7209dd8d9a5d70149270
Nicolai Danielsen <NICD@LB.DK>
2018-07-27 08:10:20 +0200

    ændret main-ps1 og tilføjet bygcsv

--- Powershell/MigratePages/Main.ps1
+++ Powershell/MigratePages/Main.ps1
@@ -173,6 +173,7 @@ function ProcesFile($fileName)
                 #$url = [uri]::EscapeDataString("A conto-betaling.aspx")
                 
                 $sourceUrl= "http://intranet.lb.dk/Skade/hb/Baad/SitePages/" + $url ;
+                #$sourceUrl= "http://intranet.lb.dk/Skade/hb/BeSk/SitePages/" + $url ;
                 $targetUrl= "https://lbforsikring.sharepoint.com/sites/Skade/SitePages/" + $url ;
             
                 GetSourceFile -url $sourceUrl 
@@ -204,9 +205,12 @@ function Run($startIndex)
     try{
     
         
- 
-#$files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BaadCSVPrerun.csv -Encoding UTF8
-$files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BaadCSV.csv -Encoding UTF8
+    #$files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BaadCSVPrerun.csv -Encoding UTF8
+    #$files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BaadCSV.csv -Encoding UTF8
+    #$files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BeredskabCSVPrerun.csv -Encoding UTF8
+
+        #$files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BeredskabCSV.csv -Encoding UTF8
+        $files = Import-Csv -Path C:\Git\LBIntranet\Powershell\MigratePages\ImportFiles\BaadCSVRepair.csv -Encoding UTF8
 
 
         
