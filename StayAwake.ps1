7e2c1db208dc57f6127e389b36b76110643a7dcc
Nicolai Danielsen <NICD@LB.DK>
2018-08-10 09:32:42 +0200

    ..

---
+++ StayAwake.ps1
@@ -0,0 +1,11 @@
+﻿param($minutes = 6000)
+
+write "... screen will be awake for $minutes"
+
+ $myshell = New-Object -com "Wscript.Shell"
+
+ for ($i = 0; $i -lt $minutes; $i++) {
+ write "... screen will be awake for" ($minutes-$i)
+ Start-Sleep -Seconds 60    
+ $myshell.sendkeys("{F15}")
+}
