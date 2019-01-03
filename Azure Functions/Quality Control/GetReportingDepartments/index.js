2cb68104546ab4a1f10928dc1c04758854c8db5e
Nicolai Danielsen <NICD@LB.DK>
2019-01-03 15:53:49 +0100

    'qq'

--- Azure Functions/Quality Control/GetReportingDepartments/index.js
+++ Azure Functions/Quality Control/GetReportingDepartments/index.js
@@ -1,9 +1,30 @@
+const SPFetchClient = require("@pnp/nodejs").SPFetchClient;
+const sp =  require("@pnp/sp").sp;
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
+
+        const siteName = req.query.site || req.body.site;
+        // sp.setup({
+        //     sp: {
+        //         fetchClientFactory: () => {
+        //             return new SPFetchClient(
+        //               `https://lbforsikring.sharepoint.com/sites/Skade/`, 
+        //               process.env.spId, 
+        //               process.env.spSecret);
+        //         },
+        //     },
+        // });
+
+        // Get the web and all the Lists
+        // const web = await sp.web.select("Title").expand('Lists').get();
+        const list = await sp.web("Skade").lists.getByTitle('LeaderGroup').items.select("Title");
+
+
         context.res = {
             // status: 200, /* Defaults to 200 */
+            
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
