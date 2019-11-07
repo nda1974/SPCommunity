8d5eb11d9febf0acab09329d3bd0be2d9c60890c
Nicolai Danielsen <NICD@LB.DK>
2019-11-07 13:43:26 +0100

    adf

---
+++ poc-devops/src/webparts/pocDevOps/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPocDevOpsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PocDevOpsWebPartStrings' {
+  const strings: IPocDevOpsWebPartStrings;
+  export = strings;
+}
